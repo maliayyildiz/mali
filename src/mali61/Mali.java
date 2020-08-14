43b2f0c8a237c6a409f2c8e14193a8befa6bc675
maliayyildiz <69386343+maliayyildiz@users.noreply.github.com>
2020-08-15 01:01:27 +0200

    Update Mali.java
    1 remote repo

--- src/mali61/Mali.java
+++ src/mali61/Mali.java
@@ -4,7 +4,8 @@ public class Mali {
  public static void main(String[] args) {
 	System.out.println("1 from local repo");
 	System.out.println("2nd local repo"); 
-	 
+	
+	System.out.println("1 from remote repo"); 
 	 
 	 
 	 
